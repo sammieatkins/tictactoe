e7641171a6679cd34ee9bb794fbb19c8df39ffbe
sammieatkins <104380965+sammieatkins@users.noreply.github.com>
2023-01-22 19:25:05 -0700

    practice

---
+++ header_practice/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './Title';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <img src={logo} />
+    </div>
+  );
+}
+
+export default App;
---
+++ header_practice/src/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className='Title'>
+      <h1>SFPOPOS</h1>
+      <h2 className='Title-Subtitle'>San Franciscos Privately Owned Public Spaces</h2>
+    </div>
+  )
+}
+
+export default Title
